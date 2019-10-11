c218305677b6d8baf9887b54a90ac11d98435229
GenerGener <56448331+GenerGener@users.noreply.github.com>
2019-10-11 16:32:18 -0500

    Update Sample.py

---
+++ modules/Storage/Sample.py
@@ -0,0 +1,14 @@
+"""assume sort VCF after header, uniq"""
+
+class Sample(object):
+    """ Sample name """
+
+    def __init__(self, sample_name):
+        self.sample_name = sample_name
+        self.chrom_name = []
+
+    def add.chrom_name(chrom_name):
+        if chrom_name in self.chrom_name
+            pass
+        else: ChromName = chrom_name.chrom_name
+            chrom_name = chrom_name
