6bd17ebfc0024fd09a19f0d52bd7ecde940d98f3
GenerGener <56448331+GenerGener@users.noreply.github.com>
2019-10-11 16:36:53 -0500

    Update Sample.py

--- modules/Storage/Sample.py
+++ modules/Storage/Sample.py
@@ -7,8 +7,8 @@ class Sample(object):
         self.sample_name = sample_name
         self.chrom_name = []
 
-    def add.chrom_name(chrom_name):
+    def add__chrom_name(chrom_name):
         if chrom_name in self.chrom_name
             pass
-        else: ChromName = chrom_name.chrom_name
+        else ChromName = chrom_name.chrom_name
             chrom_name = chrom_name
