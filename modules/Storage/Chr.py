30272aeb1dcd070867983fe3d5457685c66b61d7
Venkat Malladi <venkat.malladi@utsouthwestern.edu>
2019-10-11 15:46:50 -0500

    Update position of chromosome.

--- modules/Storage/Chr.py
+++ modules/Storage/Chr.py
@@ -1,12 +1,18 @@
 class Chr(object):
     """ Chromosome Variant object """
 
-    def __init__(self, chrom_name, start, end):
-        self.chrom_name = chrom_name
-        self.start = start
-        self.end = end
+    def __init__(self, chrom_name, pos):
+        self.chrom_name = str()
+        self.start = pos
+        self.end = pos
         self.variants = {}
-        
+
+    def update_position(self, pos):
+        if self.start > pos:
+            self.start = pos
+        elif self.end < pos:
+            self.end = pos
+
     def add_variant(self, variant):
         if variant in self.variants:
             self.variants[variant] += 1
