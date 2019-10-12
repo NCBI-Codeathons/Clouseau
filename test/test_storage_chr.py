dcdcb3c5bd5271a2c2cb9be9f355b1a0a8343e6b
Venkat Malladi <venkat.malladi@utsouthwestern.edu>
2019-10-12 15:03:33 -0500

    Test add variant.

--- test/test_storage_chr.py
+++ test/test_storage_chr.py
@@ -9,6 +9,13 @@ def default_chr():
     return chr_dict
 
 
+@pytest.fixture
+def default_chr_1(default_chr):
+    chr_dict = default_chr
+    chr_dict['variant_type'] = 'variant1'
+    return chr_dict
+
+
 @pytest.mark.unit
 def test_init_chromosome(default_chr):
     chrom_name = default_chr['name']
@@ -18,3 +25,14 @@ def test_init_chromosome(default_chr):
     assert new_chr.start == 25
     assert new_chr.end == 25
     assert len(new_chr.variants) == 0
+
+
+@pytest.mark.unit
+def test_add_variant(default_chr_1):
+    chrom_name = default_chr_1['name']
+    position = default_chr_1['position']
+    new_chr = Chr.Chr(chrom_name, position)
+    variant_type = default_chr_1['variant_type']
+    new_chr.add_variant(variant_type) 
+    assert len(new_chr.variants) == 1
+    assert new_chr.variants['variant1'] == 1
