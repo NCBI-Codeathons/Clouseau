6bdb94c2b08d6cbb9a594a29ba127d021241732e
Arkarachai <arkarachai.af@gmail.com>
2019-10-12 17:45:35 -0500

    add gender check

--- modules/VCFProcess/qc_calculation.py
+++ modules/VCFProcess/qc_calculation.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 
-from Storage import Sample
-from Storage import Chr
-from Storage import AllSamples
+#from Storage import Sample
+#from Storage import Chr
+#from Storage import AllSamples
+
+
+def remove_prefix(text, prefix):
+    return text[text.startswith(prefix) and len(prefix):]
 
 def chromosome_check(all_sample):
     """
@@ -19,3 +23,23 @@ def number_of_sample(all_sample):
     """
     return len(sample_list)
 
+def gender_check(sample):
+    """
+    gender_check(Sample) -> character
+    :return: M or F representing gender for individual samples
+    """
+    MALE_SIGNATURE = ['Y','y']
+
+    chromosome_list = sample.chr_name
+    #chromosome_list=['chr1','chr2']
+    chromosome_list = map(lambda x: remove_prefix(x,'chr'), chromosome_list)
+    chromosome_list = map(lambda x: remove_prefix(x, 'Chr'), chromosome_list)
+    first_chr_chrommosome_list = set(map(lambda x: x[0], chromosome_list))
+    if len(MALE_SIGNATURE.intersection(first_chr_chrommosome_list)) >0:
+        return 'M'
+    else:
+        return 'F'
+
+
+if __name__ == '__main__':
+    gender_check(2)
