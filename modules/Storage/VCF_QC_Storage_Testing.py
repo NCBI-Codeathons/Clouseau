c4dd3a19c46119f47c3d1bd829467f5ce6a3d175
Robert Fullem <bobbyfullem@gmail.com>
2019-10-12 14:24:59 -0500

    Update the Unit Test File For Storage Classes

--- modules/Storage/VCF_QC_Storage_Testing.py
+++ modules/Storage/VCF_QC_Storage_Testing.py
@@ -16,14 +16,6 @@ class Test(unittest.TestCase):
     chr_variants = {}
     chr_objs = []
 
-    # if chr_name in self.chr_list:
-    #     chr_name.update_position(position)
-    #     chr_name.add_variant(variant)
-    # else:
-    #     chr_name = Chr.Chr(chr_name, position)
-    #     self.chr_list.append(chr_name)
-    #     chr_name.add_variant(variant)
-  
 
     # test case function to check the Chr init
     def test_0_init_chr(self):
@@ -49,12 +41,43 @@ class Test(unittest.TestCase):
             self.assertIsInstance(chr_obj.end, int)
             self.assertIsInstance(chr_obj.chrom_name, str)
 
-    def test_1_add_variant_chr(self):
-        for i in range(4):
-            self.position2 = int(math.exp(randint(1, 5))) * 30
-            chr_obj =
 
-    def test_2_update_chr(self):
+    def test_1_add_variant_chr(self):
+        print("Start Chr Class add_variant test\n")
+        #loop through a few times to check adding variant
+        chr_obj = self.chr_objs[1]
+        for i in range(4):
+            #dummy values
+            if i == 3:
+                chr_obj.add_variant("variant1")
+            else:
+                chr_obj.add_variant("variant" + str(i))
+        for key, value in chr_obj.variants.items():
+            # print(key, value)
+            self.assertIsInstance(key, str)
+            self.assertTrue(value >= 1)
+        self.assertIs(chr_obj.variants["variant1"], 2)
+        i = len(chr_obj.variants.keys())
+        #check a couple special conditions dont add to dic
+        chr_obj.add_variant(None)
+        print("Check nested str not added to dic")
+        with self.assertRaises(TypeError):
+            chr_obj.add_variant(["test_variant"])
+        # for key, value in chr_obj.variants.items():
+        #     print(key, value)
+        print("Check that null key not added to dic")
+        self.assertEqual(i, len(chr_obj.variants.keys()))
+
+    # if chr_name in self.chr_list:
+    #     chr_name.update_position(position)
+    #     chr_name.add_variant(variant)
+    # else:
+    #     chr_name = Chr.Chr(chr_name, position)
+    #     self.chr_list.append(chr_name)
+    #     chr_name.add_variant(variant)
+
+    def test_2_update_position_chr(self):
+        print("Start Chr Class update position test\n")
         for i in range(6):
             # check if update position works
             chr_obj =
